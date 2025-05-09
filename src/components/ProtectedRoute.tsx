5a59251ff790165f77aef0c69e77cfffc2c43ba2
Nomin Sendinu <soloriderns@gmail.com>
2025-05-09 13:10:18 +0530

    add:login warn to protected routes

--- src/components/ProtectedRoute.tsx
+++ src/components/ProtectedRoute.tsx
@@ -1,10 +1,12 @@
 // src/components/ProtectedRoute.js
 import React from "react";
 import { Navigate, Outlet } from "react-router-dom";
-import { useAuth } from "../contexts/AuthContext";
+import { useAuth } from "../contexts/authContext";
+import { useToast } from "@/hooks/use-toast";
 
 export function ProtectedRoute({ requireAdmin = false }) {
   const { currentUser, loading } = useAuth();
+  const { toast } = useToast();
 
   if (loading) {
     // You can replace this with a loading spinner component
@@ -13,6 +15,11 @@ export function ProtectedRoute({ requireAdmin = false }) {
 
   // Check if user is authenticated
   if (!currentUser) {
+    console.log("Cart Protect Hit");
+    toast({
+      title: "Please Log In",
+      description: "You're not logged in, Please log in to continue with the purchase",
+    });
     // Redirect to login if not authenticated
     return <Navigate to="/" replace />;
   }
